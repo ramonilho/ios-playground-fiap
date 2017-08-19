5d8883f10d7b7874a720cad3d8a324f003a74ddc
Ramon Honorio <ramonaqh@gmail.com>
2017-08-19 12:18:09 -0300

    custom operators, enums, n parameter functions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -65,11 +65,11 @@ print(anyTypeArray)
 
 for item in anyTypeArray {
     if item is Int {
-        print("\(item) is Int")
+        print("\(item!) is Int")
     } else if item is String {
-        print("\(item) is String")
+        print("\(item!) is String")
     } else if item == nil {
-        print("\(item) is nil")
+        print("item is nil")
     }
 }
 
@@ -105,23 +105,109 @@ states.map { print($0) }
 
 print("================================")
 
-// --------------------------- //
-var str = "swift rules"
-let coolBlue = UIColor(red: 52/255.0, green: 73/255.0, blue: 94/255.0, alpha: 1.0)
 
-// Label Setup
-let awesomeLabel = UILabel(frame: CGRect(x: 0, y: 66, width: 200, height: 44))
-awesomeLabel.text = str
-awesomeLabel.textAlignment = NSTextAlignment.center
-awesomeLabel.textColor = UIColor.white
+// Operadores personalizados
+// Operador prefix: opera antes do operando
+prefix operator >-
+prefix func >- (right: Int) -> Int {
+    return right * right * (-1)
+}
+>-5
 
-// Label Style
-awesomeLabel.backgroundColor = coolBlue
-awesomeLabel.layer.masksToBounds = true
-awesomeLabel.layer.cornerRadius = 10.0
+// Operador postfix: opera depois do operando
+postfix operator -<
+postfix func -< (right: Int) -> Int {
+    return right / right * (-1)
+}
+5-<
 
-// Adding into a view
-var view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-view.addSubview(awesomeLabel)
+// Operador infix: opera entre dois valores
+infix operator >-<
+func >-< (left: Int, right: Int) -> Int {
+    return 2 * left * right
+}
+3>-<5
 
-// --------------------------- //
+
+// Desafio print( 15 <-> 25 )
+import Foundation
+infix operator <->
+func <-> (numberOfElements: Int, max: Int) -> [Int] {
+    var result = [Int]()
+    
+    while result.count < numberOfElements {
+        let random = Int(arc4random_uniform(UInt32(max)))+1
+        if !result.contains(random) {
+            result.append(random)
+        }
+    }
+    
+    return result.sorted()
+}
+
+
+print(6<->60)
+
+// Função com n parâmetros
+func sumValues(_ initialValue: Int, with values:Int..., multipier: Int = 1) -> Int{
+    var result = initialValue
+    for value in values {
+        result += value
+    }
+    return result * multipier
+}
+sumValues(0, with: 2,3,4,5,6,7,10)
+sumValues(1, with: 2,3,4,5,6,7,10, multipier: 2)
+
+enum OperationType {
+    case sum
+    case substract
+    case multiply
+    case divide
+}
+
+// First Class Citizen || Clojures
+typealias Operation = (Int, Int) -> Int
+
+func sum(_ a: Int,_ b:Int) -> Int{
+    return a + b
+}
+func substract(_ a: Int,_ b:Int) -> Int{
+    return a - b
+}
+func multiply(_ a: Int,_ b:Int) -> Int{
+    return a * b
+}
+func divide(_ a: Int,_ b:Int) -> Int{
+    return a / b
+}
+
+func calculate(_ a: Int,_ b:Int, operation: OperationType) -> Int {
+    switch operation {
+        case .sum:
+            return sum(a, b)
+        case .substract:
+            return substract(a, b)
+        case .divide:
+            return divide(a, b)
+        case .multiply:
+            return multiply(a, b)
+    }
+    
+}
+
+calculate(2, 3, operation: .multiply)
+
+// Resumindo funções
+func calc(_ a: Int, _ b: Int, operation: Operation) -> Int {
+    return operation(a, b)
+}
+calc(2, 3) { (a, b) -> Int in
+    return a * b
+}
+calc(2, 3) {
+    return $0 * $1
+}
+calc(2, 3) { $0 * $1 }
+
+
